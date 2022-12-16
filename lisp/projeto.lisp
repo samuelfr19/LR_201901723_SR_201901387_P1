72015eab0b7850acc92004b918617f238d9f5e77
samuelfr19 <samuelferreira2001@gmail.com>
2022-12-16 23:28:16 +0000

    minor updates

--- lisp/projeto.lisp
+++ lisp/projeto.lisp
@@ -1,7 +1,7 @@
 "ficheiro que carrega os outros ficheiros de codigo, escreve e le ficheiros, e trata da interacao com o utilizador"
 
-(load "./lisp/procura.lisp")
-(load "./lisp/puzzle.lisp")
+(load "procura.lisp")
+(load "puzzle.lisp")
 
 (defun startMessage()
 "Mostra as opções iniciais)"
@@ -15,21 +15,30 @@
     (format t "~%'   3        Exit              '")
     (format t "~%'                              '")
     (format t "~% ''''''''''''''''''''''''''''''    ~%~%>")
- )
+  )
 )
 
 ;; <solution>::= (<start-time> <solution-path> <end-time> <numero-board> <algorithm>)
 (defun start()
   (progn
-    (start-message)
+    (startMessage)
       (let ((opt (read)))
-           (if (or (not (numberp opt)) (> opt 3) (< opt 1)) (progn (format t "") (start))
+           (if (or (not (numberp opt)) (> opt 3) (< opt 1)) (progn (format t "Por favor escolha uma opcao possivel!~%") (start))
            (ecase opt
-              ('1 (format t "OPÇÃO 1"))
-              ('2 (format t "OPÇÃO 2"))
-              ('3 (format t "OPÇÃO 3"))
+              ('1 (format t "OPCAO 1"))
+              ('2 (format t "OPCAO 2"))
+              ('3 (format t "OPCAO 3"))
            )
            )
-      )
+    )
+  )
+)
+
+(defun algorithmMessage()
+  (progn
+    (format t "~%Escolha o argoritmo de procura que pretende utilizar~% ")
+    (format t "~%         1   Breadth first search")
+    (format t "~%         2    Depth first search")
+    (format t "~%         3            A*~%~%")
   )
 )
