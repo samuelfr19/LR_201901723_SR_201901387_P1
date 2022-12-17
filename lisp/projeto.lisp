934834347e41e66cdb0935b3e5154197c48772b4
LuisRocha <lmcr666@gmail.com>
2022-12-17 02:31:05 +0000

    reading from file, unfinished

--- lisp/projeto.lisp
+++ lisp/projeto.lisp
@@ -1,27 +1,28 @@
 "ficheiro que carrega os outros ficheiros de codigo, escreve e le ficheiros, e trata da interacao com o utilizador"
 
-(load "./lisp/procura.lisp")
-(load "./lisp/puzzle.lisp")
+(load "procura.lisp")
+(load "puzzle.lisp")
+(require "asdf")
 
 (defun startMessage()
 "Mostra as opções iniciais)"
   (progn
-    (format t "~% ''''''''''''''''''''''''''''''")
-    (format t "~%'          Dots and            '")
-    (format t "~%'            Boxes             '")
-    (format t "~%'                              '")
-    (format t "~%'   1 Visualizar Problemas     '")
-    (format t "~%'   2  Escolher problema       '")
-    (format t "~%'   3        Exit              '")
-    (format t "~%'                              '")
-    (format t "~% ''''''''''''''''''''''''''''''    ~%~%>")
+    (format t "~%--------------------------------")
+    (format t "~%|          Dots and            |")
+    (format t "~%|            Boxes             |")
+    (format t "~%|                              |")
+    (format t "~%|   1 Visualizar Problemas     |")
+    (format t "~%|   2  Escolher problema       |")
+    (format t "~%|   3        Exit              |")
+    (format t "~%|                              |")
+    (format t "~% -------------------------------   ~%~%>")
  )
 )
 
 ;; <solution>::= (<start-time> <solution-path> <end-time> <numero-board> <algorithm>)
 (defun start()
   (progn
-    (start-message)
+    (startMessage)
       (let ((opt (read)))
            (if (or (not (numberp opt)) (> opt 3) (< opt 1)) (progn (format t "") (start))
            (ecase opt
@@ -31,5 +32,37 @@
            )
            )
       )
+  )
+)
+
+;----------------------------------------------------------------------------- LOADING FROM FILES
+
+(defun getProblemas ()
+  (mapcar #'
+    (lambda (line)
+      (string-to-list line)
+    )
+    (uiop:read-file-lines "../problemas.dat")
+  )
+)
+
+(defun printProblemas ()
+  (mapcar #'
+    (lambda (s) 
+      (format t s) (format t "~%")
+    )
+    (getProblemas)
+  )
+  nil
+)
+
+(defun string-to-list (str)
+  (if (not (streamp str))
+    (string-to-list (make-string-input-stream str))
+    
+    (if (listen str)
+      (cons (read str) (string-to-list str))
+      nil
+    )
   )
 )
--- lisp/puzzle.lisp
+++ lisp/puzzle.lisp
@@ -3,10 +3,10 @@
 
 (defun tabuleiroTeste ()
   "Retorna um tabuleiro 3x3 (3 arcos na vertical por 3 arcos na horizontal)"
-	'(
-        ((0 1 0) (0 1 1) (0 1 1) (0 1 1))
-        ((0 0 0) (1 1 1) (1 1 1) (0 1 1))
-     )
+  '(
+    ((0 1 0) (0 1 1) (0 1 1) (0 1 1))
+    ((0 0 0) (1 1 1) (1 1 1) (0 1 1))
+  )
 )
 
 
@@ -27,8 +27,8 @@
 	"Função que retorna o arco que se encontra numa posicao da lista de arcos
 	horizontais ou verticais"
 	(if (or (= a 0) (= b 0))
-	    NIL	
-	    (nth (- b 1) (nth (- a 1) list))
+    NIL	
+    (nth (- b 1) (nth (- a 1) list))
 	)
 )
 
@@ -49,49 +49,55 @@
 
 ;;; (checkClosedBox 1 2 (tabuleiroTeste))
 (defun checkClosedBox (a b board)
-    (and
-        "A"(= (getArcOnPosition a b (getHorizontalArcs board)) 1)
-        "B"(= (getArcOnPosition b a (getVerticalArcs board)) 1)
-        "C"(= (getArcOnPosition (+ b 1) a (getVerticalArcs board)) 1)
-        "D"(= (getArcOnPosition (+ a 1) b (getHorizontalArcs board)) 1)
-    ) 
+  (and
+    "A"(= (getArcOnPosition a b (getHorizontalArcs board)) 1)
+    "B"(= (getArcOnPosition b a (getVerticalArcs board)) 1)
+    "C"(= (getArcOnPosition (+ b 1) a (getVerticalArcs board)) 1)
+    "D"(= (getArcOnPosition (+ a 1) b (getHorizontalArcs board)) 1)
+  ) 
 )
 
 
 
 (defun horizontalArc (x y board)
-    "Função que recebe dois índices e o tabuleiro e coloca um arco horizontal nessa
-    posição. A função deverá retornar NIL caso já exista um arco colocado nessa posição ou caso a posição
-    indicada seja fora dos limites do tabuleiro."
-    (if 
-        (and (<= x (length (getHorizontalArcs board)))
-            (<= y (length (car (getHorizontalArcs board)))))
-        (if (/= (getArcOnPosition x y (getHorizontalArcs board)) 1)
-            (and
-                (arcOnPosition x y (getHorizontalArcs board)) 
-                board
-            )
-        nil
-        )
-        nil
+  "Função que recebe dois índices e o tabuleiro e coloca um arco horizontal nessa
+  posição. A função deverá retornar NIL caso já exista um arco colocado nessa posição ou caso a posição
+  indicada seja fora dos limites do tabuleiro."
+  (if 
+    (and 
+      (<= x (length (getHorizontalArcs board)))
+      (<= y (length (car (getHorizontalArcs board))))
     )
+   
+    (if (/= (getArcOnPosition x y (getHorizontalArcs board)) 1)
+      (and
+        (arcOnPosition x y (getHorizontalArcs board)) 
+        board
+      )
+    nil
+    )
+    nil
+  )
 )
  
 
 (defun verticalArc (x y board)
-    ": Função que recebe dois índices e o tabuleiro e coloca um arco vertical nessa posição.
-    A função deverá retornar NIL caso já exista um arco colocado nessa posição ou caso a posição indicada
-    seja fora dos limites do tabuleiro"
-    (if 
-        (and (<= y (length (getVerticalArcs board)))
-             (<= x (length (car (getVerticalArcs board)))))
-        (if (/= (getArcOnPosition y x (getVerticalArcs board)) 1)
-            (and
-                (arcOnPosition y x (getVerticalArcs board))
-                board
-            ) 
-        nil
-        )
-        nil
+  ": Função que recebe dois índices e o tabuleiro e coloca um arco vertical nessa posição.
+  A função deverá retornar NIL caso já exista um arco colocado nessa posição ou caso a posição indicada
+  seja fora dos limites do tabuleiro"
+  (if 
+    (and 
+      (<= y (length (getVerticalArcs board)))
+      (<= x (length (car (getVerticalArcs board))))
     )
+    
+    (if (/= (getArcOnPosition y x (getVerticalArcs board)) 1)
+      (and
+        (arcOnPosition y x (getVerticalArcs board))
+        board
+      ) 
+    nil
+    )
+    nil
+  )
 )
