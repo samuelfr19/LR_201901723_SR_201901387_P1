45c03e091f443dc1e9cbaa413876a3bfe29e9d87
samuelfr19 <samuelferreira2001@gmail.com>
2022-12-20 15:04:12 +0000

    print and getProblem updated

--- lisp/projeto.lisp
+++ lisp/projeto.lisp
@@ -3,51 +3,119 @@
 (load "procura.lisp")
 (load "puzzle.lisp")
 
+;----------------------------------------------------------------------------- MESSAGES FOR INTERFACE
 
 (defun startMessage()
 "Mostra as opções iniciais"
   (progn
-    (format t "~%--------------------------------")
+    (format t "~%~% ------------------------------")
     (format t "~%|          Dots and            |")
     (format t "~%|            Boxes             |")
     (format t "~%|                              |")
     (format t "~%|   1 Visualizar Problemas     |")
     (format t "~%|   2  Escolher problema       |")
-    (format t "~%|   3        Exit              |")
+    (format t "~%|   0        Sair              |")
     (format t "~%|                              |")
-    (format t "~% -------------------------------   ~%~%>")
+    (format t "~% ------------------------------   ~%~%> ")
+ )
+)
+
+; (algorithmMessage)
+(defun algorithmMessage()
+  (progn
+    (format t"~%~% -------------------------------------------------------------")
+    (format t "~%|    Escolha o argoritmo de procura que pretende utilizar     |")
+    (format t "~%|                                                             |")
+    (format t "~%|                1   Breadth first search                     |")
+    (format t "~%|                2    Depth first search                      |")
+    (format t "~%|                3            A*                              |")
+    (format t "~%|                0          Voltar                            |")
+    (format t "~%|                                                             |")
+    (format t"~% -------------------------------------------------------------~%~%> ")
+  )
+)
+
+;; (chooseProblemMessage)
+(defun chooseProblemMessage()
+"Mostra as opções iniciais"
+  (progn
+    (format t "~%~% ----------------------------------------")
+    (format t "~%|           Escolher Problema            |")
+    (format t "~%|                                        |")
+    (format t "~%|    Escolha o problema introduzindo     |")
+    (format t "~%|     inserindo o numero do problema     |")
+    (format t "~%|             0   Voltar                 |")
+    (format t "~%|                                        |")
+    (format t "~% ----------------------------------------   ~%~%> ")
  )
 )
 
 ;; <solution>::= (<start-time> <solution-path> <end-time> <numero-board> <algorithm>)
+; (start)
 (defun start()
   (progn
     (startMessage)
       (let ((opt (read)))
-           (if (or (not (numberp opt)) (> opt 3) (< opt 1)) (progn (format t "Por favor escolha uma opcao possivel!~%") (start))
+           (if (or (not (numberp opt)) (> opt 2) (< opt 0)) (progn (format t "Por favor escolha uma opcao possivel!~%") (start))
            (ecase opt
-              ('1 (format t "OPCAO 1"))
-              ('2 (format t "OPCAO 2"))
-              ('3 (format t "OPCAO 3"))
+              ('1 (progn (printProblems) (start)))
+              ('2 (chooseProblem))
+              ('0 (quit))
            )
            )
     )
   )
 )
 
-(defun algorithmMessage()
-  (progn
-    (format t "~%Escolha o argoritmo de procura que pretende utilizar~% ")
-    (format t "~%         1   Breadth first search")
-    (format t "~%         2    Depth first search")
-    (format t "~%         3            A*~%~%")
-  )
+;(chooseProblem)
+(defun chooseProblem()
+ (progn (chooseProblemMessage)
+      (let ((opt (read)))
+           (cond ((eq opt '0) (start))
+                 ((not (numberp opt)) 
+                  (progn (format t "Insira uma opcao valida") (chooseProblem))) 
+                 (T
+                  (let ((problem-list (getproblems)))
+                  (if (or (< opt 0) (> opt (length problem-list))) (progn (format t "Insira uma opcao valida") (chooseProblem))
+                                 (nth(1- opt) problem-list)
+                  )))
+           )
+       )
+    )
 )
+
+
+
+
+
+;; (print-board (tabuleiroTeste)))
+(defun print-board(board &optional (stream t))
+  "Mostra um tabuleiro bem formatado"
+  (not (null (mapcar #'(lambda(l) (format stream "~%~t~t ~a" l)) board)))
+  (format t "~%-------------------------------------------------------------------------------------------------------------------------------------------~%")
+)
+
+;; (printProblems)
+(defun printProblems (&optional (i 1) (problems (getProblems)))
+    (cond
+     ((null problems) 
+        
+     )
+     (T (let ((problem (car problems)))
+        (format T "   ~d- Tabuleiro ~d (~d caixas):~%" i (car problem) (cadr problem))
+        (print-board (caddr problem)))
+
+
+        (printproblems (+ i 1) (cdr problems))
+     ) 
+)
+)
+
 
 ;----------------------------------------------------------------------------- LOADING FROM FILES
 
-;; (getProblemas)
-(defun getProblemas ()
+;; (getProblems)
+(defun getProblems ()
   "Metodo para ir buscar os problemas a partir do ficheiro 'problemas.dat'"
   (with-open-file (stream "../problemas.dat" :if-does-not-exist :error)
     (do 
@@ -60,30 +128,15 @@
   )
 )
 
-;; (getProblema "e")
-(defun getProblema (n &optional (probs (getProblemas)))
+;; (getProblem "e")
+(defun getProblem (n &optional (probs (getProblems)))
   "Procurar recursivamente na lista de problemas o que estamos a procurar (todos em letra minuscula e nao passar lista) (P.E. '(getproblema \"a\")'"
   (if (car probs)
     (if (equal (first (car probs)) n)
-        (print-board (nth 2 (car probs)))
-      (getProblema n (cdr probs))
+        (car probs)
+      (getProblem n (cdr probs))
     )
   )
-)
-
-;; (print-board (nth 2 (getProblema "b")))
-;; (print-board (tabuleiroTeste)))
-(defun print-board(board &optional (stream t))
-  "Mostra um tabuleiro bem formatado"
-  (not (null (mapcar #'(lambda(l) (format stream "~%~t~t ~a" l)) board)))
-)
-
-;; (printProblems)
-(defun printProblems ()
-  (mapcar #'(lambda (problema &aux (board problema))  (progn (format T "~%~%Tabuleiro ~d:~%" (car board))
-                                                             (print-board (caddr board)))) 
-                                                             (getProblemas))
-  T
 )
 
  
